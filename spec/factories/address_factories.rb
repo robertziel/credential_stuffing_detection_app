fe462431b41c0e09071da8091991e240ff47536e
Robert Zieliński <hello@robertz.co>
2020-08-03 08:56:24 +0200

    Address ban! specs

--- spec/factories/address_factories.rb
+++ spec/factories/address_factories.rb
@@ -1,4 +1,5 @@
 FactoryBot.define do
   factory :address do
+    ip { Faker::Internet.ip_v4_address }
   end
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 describe Address do
+  let(:address) { create :address }
+
   describe '#associations' do
     it { should have_many(:events).dependent(:destroy) }
   end
 
   describe '#ban!' do
-    it ''
+    subject do
+      address.ban!
+    end
+
+    it 'sets banned_at' do
+      address.update_column(:banned_at, nil)
+      subject
+      expect(address.banned_at).not_to eq nil
+    end
   end
 end
